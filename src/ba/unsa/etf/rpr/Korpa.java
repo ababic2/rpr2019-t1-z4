289bdd800d89dce5a668422bc6d96a771fd5f3dd
ababic2 <ababic2@etf.unsa.ba>
2019-10-20 19:30:01 +0200

    Urađen zadatak

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,44 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+
+    private Artikl[] artikli;
+    private int broj = 0;
+
+    public Korpa() {
+        artikli = new Artikl[50];
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for (int i = 0; i < broj; i++) suma = suma + artikli[i].getCijena();
+        return suma;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i = 0; i < broj; i += 1) {
+            if (artikli[i].getKod().equals(kod)) {
+                Artikl a = new Artikl(artikli[i].getNaziv(), artikli[i].getCijena(), artikli[i].getKod());
+                for (int j = i; j < artikli.length - 1; j += 1) {
+                    artikli[j] = artikli[j + 1];
+                }
+                broj--;
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+        if (broj < 50) {
+            artikli[broj] = a;
+            broj++;
+            return true;
+        }
+        return false;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] supermarket;
+    private int broj_artikala = 0;
+
+    public Supermarket() {
+        supermarket = new Artikl[1000];
+    }
+
+    public void dodajArtikl(Artikl a) {
+        if (broj_artikala < 1000) {
+            supermarket[broj_artikala] = a;
+            broj_artikala++;
+        }
+    }
+
+    public Artikl[] getArtikli() {
+        return supermarket;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i = 0; i < broj_artikala; i ++) {
+            if (supermarket[i].getKod().equals(kod)) {
+                Artikl a = supermarket[i];
+                broj_artikala--;
+                if (supermarket.length - 1 - i >= 0)
+                    System.arraycopy(supermarket, i + 1, supermarket, i, supermarket.length - 1 - i);
+                return a;
+            }
+        }
+        return null;
+    }
+
 }
